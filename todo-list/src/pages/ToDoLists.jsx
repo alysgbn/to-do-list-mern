e0c934443156eb2f6c1314ff8f36d565dbe0e60e
aliyahsagaban <aliyah.sagaban@aiqueinnovation.com>
2024-09-23 17:54:18 +0800

    changed styling and layout for edit funcitonality

--- todo-list/src/pages/ToDoLists.jsx
+++ todo-list/src/pages/ToDoLists.jsx
@@ -1,18 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-const ToDoLists = ({ todoList }) => {
-    console.log("list", todoList)
+const ToDoLists = ({ todoList, deleteTask, editTask }) => {
+  const [edit, setEdit] = useState(false);
+  console.log(edit);
   return (
     <div>
       <h1>To Do List</h1>
       {todoList?.map((todo, index) => (
         <div className="todo-lists" key={index}>
-          <Button variant="outline-light">Edit</Button>{" "}
           <div>
             <h5>Task {index + 1}</h5>
-            <p>{todo}</p>
+            {edit ? (
+              <input
+                type="text"
+                value={todo}
+                onChange={(e) => editTask(index, e.target.value)}
+              />
+            ) : (
+              <p>{todo}</p>
+            )}
           </div>
-          <Button variant="outline-light">Delete</Button>{" "}
+          <div className="edit-delete">
+            <Button
+              key={index}
+              variant="outline-light"
+              onClick={() => {
+                editTask(index, todo);
+                setEdit(!edit);
+              }}
+            >
+              {edit ? "Save" : "Edit"}
+            </Button>{" "}
+            <Button variant="outline-light" onClick={() => deleteTask(index)}>
+              Delete
+            </Button>
+          </div>
         </div>
       ))}
     </div>
