fa18c9d41bb787ffbc791660b3c2d378b56db223
aliyahsagaban <aliyah.sagaban@aiqueinnovation.com>
2024-09-23 15:32:39 +0800

    added react bootstrap and sass

--- todo-list/src/pages/Home.jsx
+++ todo-list/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "../resources/home.scss";
+import { Button, Form } from "react-bootstrap";
 
 export const Home = () => {
   return (
-    <>
-      <div>To Do List</div>
-      <p>try lang kung anong ilalagay</p>
-    </>
+    <body>
+      <h1>To Do List</h1>
+      <div className="form-btn">
+        <Form.Label>To-do</Form.Label>
+        <Form>
+          <Form.Group controlId="todo">
+            <Form.Control type="text" placeholder="Input to-do" />
+          </Form.Group>
+        </Form>
+        <Button variant="outline-light">Add</Button>{" "}
+      </div>
+    </body>
   );
 };
