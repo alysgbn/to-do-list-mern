73ad819e36ed484e26f9e2b04b8e5dbd57e1387f
aliyahsagaban <aliyah.sagaban@aiqueinnovation.com>
2024-09-23 15:01:59 +0800

    initial commit homepage branch

--- todo-list/src/pages/Home.jsx
+++ todo-list/src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export const Home = () => {
   return (
-    <div>To Do List</div>
-  )
-}
+    <>
+      <div>To Do List</div>
+      <p>try lang kung anong ilalagay</p>
+    </>
+  );
+};
