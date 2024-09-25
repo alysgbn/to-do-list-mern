98e9315c17b107ee50d81a267d04014ac37973f1
aliyahsagaban <aliyah.sagaban@aiqueinnovation.com>
2024-09-25 17:22:49 +0800

    added edit functionality

--- todo-list/src/pages/ToDoLists.jsx
+++ todo-list/src/pages/ToDoLists.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 const ToDoLists = ({ todoList, deleteTask, editTask }) => {
-  const [edit, setEdit] = useState(false);
-  console.log(edit);
+  const [editIndex, setEditIndex] = useState(null);
+  const [saved, setSaved] = useState(false);
   return (
     <div>
       <h1>To Do List</h1>
@@ -10,7 +10,7 @@ const ToDoLists = ({ todoList, deleteTask, editTask }) => {
         <div className="todo-lists" key={index}>
           <div>
             <h5>Task {index + 1}</h5>
-            {edit ? (
+            {editIndex === index && !saved ? (
               <input
                 type="text"
                 value={todo}
@@ -25,11 +25,16 @@ const ToDoLists = ({ todoList, deleteTask, editTask }) => {
               key={index}
               variant="outline-light"
               onClick={() => {
-                editTask(index, todo);
-                setEdit(!edit);
+                setEditIndex(index);
+
+                if (editIndex === index) {
+                  editTask(index, todo);
+                  setSaved(true);
+                }
+                setSaved(!saved);
               }}
             >
-              {edit ? "Save" : "Edit"}
+              {editIndex === index && !saved ? "Save" : "Edit"}
             </Button>{" "}
             <Button variant="outline-light" onClick={() => deleteTask(index)}>
               Delete
